eb29ed2de6d508cc19a15e5636d0f14c4e4f31c9
Nirmalya Mondal <mondalnirmalya147@gmail.com>
2026-09-23 14:09:19 +0530

    Fix typo in comment about production baseline

--- Examples/Resources/AADActivityBasedTimeoutPolicy/2-Update.ps1
+++ Examples/Resources/AADActivityBasedTimeoutPolicy/2-Update.ps1
@@ -1,6 +1,6 @@
 <#
 This example is used to test new resources and showcase the usage of new resources being worked on.
-It is not meant to use as a production baseline.
+It is not meant to be used as a production baseline.
 #>
 
 Configuration Example
